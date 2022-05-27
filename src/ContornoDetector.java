49c01768e2bfdf6576d1c3e28169a13d56446702
elLukitas <alufer7440@ieselcaminas.org>
2022-05-27 17:41:46 +0200

    asa

--- src/ContornoDetector.java
+++ src/ContornoDetector.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ContornoDetector {
@@ -11,53 +12,102 @@ public class ContornoDetector {
     int threshold;
     int numeroPixelesDiferentes;
 
-    public ContornoDetector(File file, int threshold, int numeroPixelesDiferentes) {
+    public ContornoDetector(File file, int threshold) {
         if (file.exists()) {
             this.file = file;
             this.threshold = threshold;
-            this.numeroPixelesDiferentes = numeroPixelesDiferentes;
+
         }
     }
 
-    public void detectarContorno() {
-        File grey = blureador(file,4);
+    public void detectarContorno(int nivel) {
+
+        File grey = blureador(file, nivel);
         ArrayList<int[]> lista = new ArrayList<>();
+
+
         try {
             BufferedImage img = ImageIO.read(grey);
-            BufferedImage orignal = ImageIO.read(file);
+
             for (int i = 0; i < img.getWidth(); i++) {
                 for (int j = 0; j < img.getHeight(); j++) {
                     int reference = img.getRGB(i, j);
                     Color color = new Color(reference);
-                    int numeroPixeles = 0;
-                    for (int k = -1; k < 2; k++) {
-                        for (int l = -1; l < 2; l++) {
-                            int nuevo = rgb(img, i + k, j + l);
-                            if (nuevo != -1) {
-                                Color newColor = new Color(nuevo);
-                                if (Math.abs(color.getRed() - newColor.getRed()) > threshold || Math.abs(color.getBlue() - newColor.getBlue()) > threshold || Math.abs(color.getGreen() - newColor.getGreen()) > threshold) {
-                                    numeroPixeles++;
-                                    if (numeroPixeles >= numeroPixelesDiferentes) {
-                                        int[] pixelesDeI = {i, j};
-                                        lista.add(pixelesDeI);
 
-                                    }
-                                }
-                            }
-                        }
+                    int[] position = {i, j};
+
+                    int menosmenos = rgb(img, i - 1, j - 1);
+                    Color un = new Color(menosmenos);
+                    int ceromenos = rgb(img, i, j - 1);
+                    Color dos = new Color(ceromenos);
+                    int unomenos = rgb(img, i + 1, j - 1);
+                    Color tres = new Color(unomenos);
+                    int menoscero = rgb(img, i - 1, j);
+                    Color cuatro = new Color(menoscero);
+                    int unocero = rgb(img, i + 1, j);
+                    Color cinco = new Color(unocero);
+                    int menosuno = rgb(img, i - 1, j + 1);
+                    Color seis = new Color(menosuno);
+                    int cerouno = rgb(img, i, j + 1);
+                    Color siete = new Color(cerouno);
+                    int ununo = rgb(img, i + 1, j + 1);
+                    Color ocho = new Color(ununo);
+
+
+                    if (esDiferent(color, un, threshold) && esDiferent(color, dos, threshold) && esDiferent(color, tres, threshold)) {
+                        lista.add(position);
+
+                    } else if (esDiferent(color, un, threshold) && esDiferent(color, dos, threshold) && esDiferent(color, cuatro, threshold)) {
+                        lista.add(position);
+
+                    } else if (esDiferent(color, tres, threshold) && esDiferent(color, dos, threshold) && esDiferent(color, cinco, threshold)) {
+                        lista.add(position);
+
+                    } else if (esDiferent(color, un, threshold) && esDiferent(color, cuatro, threshold) && esDiferent(color, seis, threshold)) {
+                        lista.add(position);
+
+                    } else if (esDiferent(color, seis, threshold) && esDiferent(color, cuatro, threshold) && esDiferent(color, siete, threshold)) {
+                        lista.add(position);
+
+                    } else if (esDiferent(color, seis, threshold) && esDiferent(color, siete, threshold) && esDiferent(color, ocho, threshold)) {
+                        lista.add(position);
+
+                    } else if (esDiferent(color, tres, threshold) && esDiferent(color, cinco, threshold) && esDiferent(color, ocho, threshold)) {
+                        lista.add(position);
+
+                    } else if (esDiferent(color, siete, threshold) && esDiferent(color, ocho, threshold) && esDiferent(color, cinco, threshold)) {
+                        lista.add(position);
+
                     }
+                }
 
 
+            }
+
+            BufferedImage contornoPuro = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+            for (int i = 0; i < contornoPuro.getWidth(); i++) {
+                for (int y = 0; y < contornoPuro.getHeight(); y++) {
+                    contornoPuro.setRGB(i, y, Color.white.getRGB());
                 }
             }
             for (int[] posicion : lista) {
-                orignal.setRGB(posicion[0], posicion[1], Color.red.getRGB());
+                img.setRGB(posicion[0], posicion[1], Color.red.getRGB());
+                contornoPuro.setRGB(posicion[0], posicion[1], Color.black.getRGB());
             }
             File fileModified = new File("contornoPillao.jpg");
-            ImageIO.write(orignal, "jpg", fileModified);
+            ImageIO.write(img, "jpg", fileModified);
+            File fileContorno = new File("contornoPuro.jpg");
+            ImageIO.write(contornoPuro, "jpg", fileContorno);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    private boolean esDiferent(Color color, Color newColor, int threshold) {
+        return Math.abs(color.getRed() - newColor.getRed()) > threshold || Math.abs(color.getBlue() - newColor.getBlue()) > threshold || Math.abs(color.getGreen() - newColor.getGreen()) > threshold;
     }
 
     private File greyConverterBuffered() {
@@ -97,11 +147,12 @@ public class ContornoDetector {
         }
         return -1;
     }
+
     private int[] red(BufferedImage img, int x, int y) {
         if (comprobator(img, x, y)) {
-            int pa= img.getRGB(x, y);
-            Color color=new Color(pa);
-            int [] colores={color.getRed(),color.getGreen(),color.getBlue()};
+            int pa = img.getRGB(x, y);
+            Color color = new Color(pa);
+            int[] colores = {color.getRed(), color.getGreen(), color.getBlue()};
             return colores;
         }
         return null;
@@ -142,63 +193,65 @@ public class ContornoDetector {
         return null;
     }
 
-    public File blureador(File file,int nivel) {
-        File grey=file;
+    public File blureador(File file, int nivel) {
+        File grey = mejorador(file);
+        if (nivel == 1) {
+            return file;
+        } else
 
 
+            try {
+                BufferedImage img = ImageIO.read(grey);
+                BufferedImage orignal = ImageIO.read(grey);
+                List<Pixelmon> lista = new ArrayList<>();
+                for (int i = 0; i < img.getWidth(); i++) {
+                    for (int j = 0; j < img.getHeight(); j++) {
+                        int r = 0;
+                        int g = 0;
+                        int b = 0;
+                        int pixels = 0;
+                        for (int k = 1 - nivel; k < nivel; k++) {
+                            for (int l = -nivel; l < nivel; l++) {
+                                int[] nuevo = red(img, i + k, j + l);
+                                if (nuevo != null) {
+                                    pixels++;
+                                    r += nuevo[0];
+                                    g += nuevo[1];
+                                    b += nuevo[2];
 
-        try {
-            BufferedImage img = ImageIO.read(grey);
-            BufferedImage orignal = ImageIO.read(file);
-            List<Pixelmon>lista=new ArrayList<>();
-            for (int i = 0; i < img.getWidth(); i++) {
-                for (int j = 0; j < img.getHeight(); j++) {
-                    int r=0;
-                    int g=0;
-                    int b=0;
-                    int pixels=0;
-                    for (int k = 1-nivel; k < nivel; k++) {
-                        for (int l = -nivel; l < nivel; l++) {
-                            int [] nuevo = red(img, i + k, j + l);
-                            if (nuevo != null) {
-                                pixels++;
-                                r+=nuevo[0];
-                                g+=nuevo[1];
-                                b+=nuevo[2];
-
+                                }
                             }
                         }
+                        int rMedia = r / pixels;
+                        int gMedia = g / pixels;
+                        int bMedia = b / pixels;
+
+                        Color color = new Color(rMedia, gMedia, bMedia);
+                        Pixelmon pixelmon = new Pixelmon(i, j, color);
+
+
+                        lista.add(pixelmon);
+
+
                     }
-                    int rMedia=r/pixels;
-                    int gMedia=g/pixels;
-                    int bMedia=b/pixels;
+                }
+                for (Pixelmon pixelmon : lista) {
 
-                    Color color=new Color(rMedia,gMedia,bMedia);
-                    Pixelmon pixelmon=new Pixelmon(i,j,color);
-
-
-                    lista.add(pixelmon);
+                    orignal.setRGB(pixelmon.getX(), pixelmon.getY(), pixelmon.getColor().getRGB());
 
 
                 }
+                File fileModified = new File("imagenBlureada.jpg");
+                ImageIO.write(orignal, "jpg", fileModified);
+                return fileModified;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            for (Pixelmon pixelmon : lista) {
-
-                        orignal.setRGB(pixelmon.getX(),pixelmon.getY(),pixelmon.getColor().getRGB());
-
-
-
-            }
-            File fileModified = new File("imagenBlureada.jpg");
-            ImageIO.write(orignal, "jpg", fileModified);
-            return fileModified;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return null;
     }
+
     public File contrastador(File file) {
-        File grey =file;
+        File grey = file;
         int contraste = 20;
         int threshold = getLuminosidad(file);
 
@@ -209,7 +262,7 @@ public class ContornoDetector {
                 for (int j = 0; j < img.getHeight(); j++) {
                     Color color = new Color(img.getRGB(i, j));
                     int red;
-                    if (color.getRed() > threshold-30) {
+                    if (color.getRed() > threshold - 30) {
                         red = color.getRed();
 
                     } else {
@@ -233,6 +286,7 @@ public class ContornoDetector {
         }
         return null;
     }
+
     public int getLuminosidad(File file) {
         File grey = file;
 
@@ -240,15 +294,15 @@ public class ContornoDetector {
         try {
             BufferedImage img = ImageIO.read(grey);
 
-            double contator=0;
+            double contator = 0;
             for (int i = 0; i < img.getWidth(); i++) {
                 for (int j = 0; j < img.getHeight(); j++) {
                     Color color = new Color(img.getRGB(i, j));
-                    contator+=color.getRed();
+                    contator += color.getRed();
 
-                    }
                 }
-            double media=contator/ (img.getHeight()* img.getWidth());
+            }
+            double media = contator / (img.getHeight() * img.getWidth());
             System.out.println(media);
 
             return (int) media;
@@ -258,18 +312,19 @@ public class ContornoDetector {
         }
         return 0;
     }
+
     public File borrator(File file) {
         File grey = file;
 
         try {
             BufferedImage img = ImageIO.read(grey);
-            int luminosidad=getLuminosidad(file);
+            int luminosidad = getLuminosidad(file);
 
             for (int i = 0; i < img.getWidth(); i++) {
                 for (int j = 0; j < img.getHeight(); j++) {
                     Color color = new Color(img.getRGB(i, j));
-                    if (color.getRed()>luminosidad+((255-luminosidad)/3)){
-                        img.setRGB(i,j,Color.white.getRGB());
+                    if (color.getRed() > luminosidad + ((255 - luminosidad) / 3)) {
+                        img.setRGB(i, j, Color.white.getRGB());
                     }
                 }
             }
@@ -284,4 +339,203 @@ public class ContornoDetector {
         return null;
     }
 
+    public File limpiatore(File file) {
+        File grey = file;
+
+
+        try {
+            BufferedImage img = ImageIO.read(grey);
+            BufferedImage orignal = ImageIO.read(file);
+            List<Pixelmon> lista = new ArrayList<>();
+            for (int i = 0; i < img.getWidth(); i++) {
+                for (int j = 0; j < img.getHeight(); j++) {
+                    int reference = img.getRGB(i, j);
+                    Color color = new Color(reference);
+                    int r = 0;
+                    int g = 0;
+                    int b = 0;
+                    int difPixels = 0;
+                    for (int k = -2; k < 3; k++) {
+                        for (int l = -2; l < 3; l++) {
+
+                            int[] nuevo = red(img, i + k, j + l);
+
+                            if (nuevo != null) {
+                                Color cloro = new Color(nuevo[0], nuevo[1], nuevo[2]);
+                                if (esDiferent(color, cloro, 5)) {
+                                    difPixels++;
+                                    r += nuevo[0];
+                                    g += nuevo[1];
+                                    b += nuevo[2];
+
+                                }
+
+                            }
+
+                        }
+                    }
+                    if (difPixels > 5) {
+                        int rMedia = r / difPixels;
+                        int gMedia = g / difPixels;
+                        int bMedia = b / difPixels;
+
+                        Color colore = new Color(rMedia, gMedia, bMedia);
+                        Pixelmon pixelmon = new Pixelmon(i, j, color);
+
+
+                        lista.add(pixelmon);
+
+                    }
+
+
+                }
+            }
+            for (Pixelmon pixelmon : lista) {
+
+                orignal.setRGB(pixelmon.getX(), pixelmon.getY(), pixelmon.getColor().getRGB());
+
+
+            }
+            File fileModified = new File("imagenLimpiada.jpg");
+            ImageIO.write(orignal, "jpg", fileModified);
+            return fileModified;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public File mejorador(File file) {
+        File grey = file;
+
+
+        try {
+            BufferedImage img = ImageIO.read(grey);
+            BufferedImage nuva = new BufferedImage(img.getWidth() * 3, img.getHeight() * 3, img.getType());
+
+            for (int i = 0; i < img.getWidth(); i++) {
+                for (int j = 0; j < img.getHeight(); j++) {
+                    Color color = new Color(img.getRGB(i, j));
+                    for (int k = -1; k < 2; k++) {
+                        for (int l = -1; l < 2; l++) {
+
+                            if (comprobator(nuva, i * 3 + k, j * 3 + l))
+                                nuva.setRGB(i * 3 + k, j * 3 + l, color.getRGB());
+
+
+                        }
+                    }
+
+                }
+
+
+            }
+            File fileModified = new File("imagenMejorada.jpg");
+            ImageIO.write(nuva, "jpg", fileModified);
+            return fileModified;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public File pixelador(File file) {
+        File grey = file;
+
+
+        try {
+            BufferedImage img = ImageIO.read(grey);
+            BufferedImage nuva = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+            for (int i = 0; i < nuva.getWidth(); i++) {
+                for (int j = 0; j < nuva.getHeight(); j++) {
+                    Color color = new Color(img.getRGB(i, j));
+
+                    for (int k = -4; k < 5; k++) {
+                        for (int l = -4; l < 5; l++) {
+
+                            if (comprobator(img, i + k, j + l)) {
+                                nuva.setRGB(i + k, j + l, color.getRGB());
+
+                            }
+                        }
+
+                    }
+
+
+                }
+
+            }
+
+            File fileModified = new File("imagenpixelada.jpg");
+            ImageIO.write(nuva, "jpg", fileModified);
+            return fileModified;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void blender(File file1) {
+        File grey = file;
+        File file=file1;
+
+
+        try {
+            BufferedImage img = ImageIO.read(grey);
+            BufferedImage orignal = ImageIO.read(file);
+            int width;
+            int heigth;
+            if (img.getWidth()>=orignal.getWidth()) {
+                 width = orignal.getWidth();
+
+            }else
+                width=img.getWidth();
+            if (orignal.getHeight()<=img.getHeight()){
+                heigth=orignal.getHeight();
+            }else{
+                heigth=img.getHeight();
+            }
+
+
+            for (int h=5;h<100;h+=5) {
+                File file2=new File("blendeada"+h/5+".jpg");
+
+                BufferedImage arpanta = new BufferedImage(width,heigth,img.getType());
+
+
+                for (int i = 0; i < arpanta.getWidth(); i++) {
+                    for (int j = 0; j < arpanta.getHeight(); j++) {
+                        int reference = img.getRGB(i, j);
+                        int newo=orignal.getRGB(i,j);
+                        Color color = new Color(reference);
+                        Color coloro=new Color(newo);
+                        int r = color.getRed()*h/100;
+                        int g = color.getGreen()*h/100;
+                        int b = color.getBlue()*h/100;
+                        int r1 = coloro.getRed()*(100-h)/100;
+                        int g1 = coloro.getGreen()*(100-h)/100;
+                        int b1 = coloro.getBlue()*(100-h)/100;
+                        int rf = r+r1;
+                        int gf = g+g1;
+                        int bf = b+b1;
+                        Color cloro=new Color(rf,gf,bf);
+                        arpanta.setRGB(i,j,cloro.getRGB());
+
+
+                    }
+
+
+                }
+                ImageIO.write(arpanta,"jpg",file2);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        File file=new File("17009.jpg");
-        ContornoDetector cd=new ContornoDetector(file,10,3);
-        cd.detectarContorno();
+    public static void main(String[] args) throws IOException {
+        File file=new File("god.jpg");
+        File file1=new File("gorila.jpg");
+        BufferedImage i= ImageIO.read(file);
+
+        ContornoDetector cd=new ContornoDetector(file,10);
+
+
+            cd.blender(file1);
+        System.out.println(i.getType());
+
     }
 }
